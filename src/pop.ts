70c3ac8f9e6e52b9eb58e8b375c3ebba49add454
BaoHaoYu <a450215151@outlook.com>
2019-11-08 09:11:40 +0800

    add: async

--- src/pop.ts
+++ src/pop.ts
@@ -2,7 +2,36 @@
 /// <reference path="../node_modules/@types/jquery/index.d.ts" />
 /// <reference path="./MESSAGE.ts" />
 
-$('.demo').on('click', function() {
+function create(createProperties: chrome.tabs.CreateProperties): Promise<chrome.tabs.Tab> {
+  return new Promise((resolve) => {
+    chrome.tabs.create(createProperties, (tab) => {
+      resolve(tab)
+    })
+  })
+}
+
+function query(queryInfo: chrome.tabs.QueryInfo): Promise<chrome.tabs.Tab[]> {
+  return new Promise((resolve) => {
+    chrome.tabs.query(queryInfo, (tabs) => {
+      resolve(tabs)
+    })
+  })
+}
+
+function sendMessage(tabId: number, message: any): Promise<any> {
+  return new Promise((resolve) => {
+    chrome.tabs.sendMessage(tabId, message, (response) => resolve(response))
+  })
+}
+
+const asyncTabs = {
+  create,
+  query,
+  sendMessage,
+}
+
+// 信息发送
+$('.send-msg').on('click', async function() {
   const $this = $(this)
 
   try {
@@ -13,8 +42,32 @@ $('.demo').on('click', function() {
     console.log(error)
   }
 })
+
+// 创建空白标签
+$('.create-empty-tag').on('click', () => {
+  chrome.tabs.create({})
+})
+
+// 在末尾创建空白标签
+$('.create-empty-tag-last').on('click', async () => {
+  chrome.tabs.query({}, (tabs) => {
+    chrome.tabs.create({ index: tabs.length })
+  })
+})
+
+// url非空白标签
+$('.create-tag-from-url').on('click', () => {
+  chrome.tabs.create({ url: chrome.extension.getURL('view/tag.html') })
+})
+
+// async wait
+$('.create-tag-from-url-async').on('click', async () => {
+  const tab = await asyncTabs.create({ url: chrome.extension.getURL('view/tag.html') })
+})
+
+// 接收信息
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request === msgObj.DEMO1) {
-    $('.demo').text(request)
+    $('.send-msg').text(request)
   }
 })
